ec5c0df9439e29ec56a7e5b68fd29a94843119a8
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-13 04:32:13 -0600

    Update foes.js

--- vr/aframe/components/foes.js
+++ vr/aframe/components/foes.js
@@ -43,6 +43,7 @@ AFRAME.registerComponent( 'foe', {
       this.el.setAttribute( 'text', 'color', "gray" );
       //this.el.object3D.children -- an array where [0] is itself, somehow.
       var children = this.el.object3D.children;
+      console.log( children.length );
       for ( i = 1; i < children.length; i++ )
         console.log( children[i] );
     }
