d1f6a8f9cd77b6ecdc6f1dde11a4cf475f4e35d1
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-16 00:48:40 -0600

    Update foes.js

--- vr/aframe/components/foes.js
+++ vr/aframe/components/foes.js
@@ -9,7 +9,9 @@ AFRAME.registerComponent( 'foe', {
   spawnNodes: function( self ) {
     for ( i = 1; i <= self.data.numNodes; i++ )
     {
-      var newCombatNodeElement = document.createElement('a-entity');
+      //var newCombatNodeElement = document.createElement('a-entity');
+      var newCombatNodeElement = self.el.sceneEl.components.pool__combatNodes.requestEntity();
+      
       var position = { x:0, y:2*i, z:0 };
       var maxNodeX = 4;
       var maxNodeY = 4;
@@ -59,7 +61,8 @@ AFRAME.registerComponent( 'foe', {
         if ( childEl != this.el )
         {
           childEl.removeAttribute( 'combat-node' ); //Stop it from ticking, else it'll crash on removal.
-          this.el.removeChild( childEl ); //Actually toss the combat-node. 
+          //this.el.removeChild( childEl ); //Actually toss the combat-node. 
+          entityEl.sceneEl.components.pool__combatNodes.returnEntity();
         }
       }
   },
