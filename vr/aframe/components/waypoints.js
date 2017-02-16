78b810a211f9ff685de9f70a1cccd6a8efd18777
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-16 02:36:12 -0600

    Update waypoints.js

--- vr/aframe/components/waypoints.js
+++ vr/aframe/components/waypoints.js
@@ -6,7 +6,7 @@ AFRAME.registerComponent( 'cursor-listener', {
     //var newCombatNodeElement = document.createElement('a-entity');
     var newWaypointElement = self.el.sceneEl.components.pool__waypoints.requestEntity();
     //Don't need to appendChild to anything here.
-    newWaypointElement.setAttribute( 'geometry', { primitive: 'cylinder', height: 1, radius: 2 } );
+    newWaypointElement.setAttribute( 'geometry', { primitive: 'cylinder', height: 0.1, radius: 1.0 } );
     newWaypointElement.setAttribute( 'material', 'color', 'yellow' );
     newWaypointElement.setAttribute( 'position', { x:hit.x, y:hit.y+1, z:hit.z } );
   },
