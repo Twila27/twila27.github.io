d1a5bc92962a0929a4b6c0c80c83a54ed60ff500
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-16 04:52:21 -0600

    Update spawning.js

--- vr/aframe/components/spawning.js
+++ vr/aframe/components/spawning.js
@@ -11,7 +11,7 @@ AFRAME.registerComponent( 'spawn-foes-on-click', {
   },
   spawn: function( self ) { 
     var newFoe = self.el.sceneEl.components.pool__foes.requestEntity(); 
-    newFoe.setAttribute( 'position', this.getRandomPosition() );
+    newFoe.setAttribute( 'position', self.getRandomPosition() );
   },
   init: function() {
     var _self = this; //Have to be sure to do this to self-ref the spawn func below.
