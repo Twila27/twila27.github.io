4b7155d861a5dacf6a3eee8ec64e4fa1f67800b3
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-16 03:54:30 -0600

    Create samsara_global.js

---
+++ vr/aframe/components/samsara_global.js
@@ -0,0 +1,13 @@
+AFRAME.registerComponent( 'samsara_global', {
+  getActiveCameraEl: function() {
+        var keysCameraEl = document.querySelector('#keysWorldCamera');
+        if ( keysCameraEl.components.camera.data.active )
+          return keysCameraEl;
+
+        var foesCameraEl = document.querySelector('#foesWorldCamera');
+        if ( foesCameraEl.components.camera.data.active )
+          return foesCameraEl;
+
+        return undefined;
+  }
+} );
