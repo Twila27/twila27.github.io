f38433eaff153cca4388b66d93a5b99001521c34
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-16 18:08:30 -0600

    Update world_swapper.js

--- vr/aframe/components/world_swapper.js
+++ vr/aframe/components/world_swapper.js
@@ -1,8 +1,9 @@
 AFRAME.registerComponent( 'world-swapper', { //Make this the mouseover-slowly-spinning Samsara logo above your head?
   swapWorlds: function( self ) {
     var manager = self.el.sceneEl.components.samsara_global;
-    var inactiveCam = manager.getInactiveCameraEl();
-    inactiveCam.setAttribute( 'camera', 'active', true ); //Should auto-shutoff active camera.
+    var inactiveAvatarEl = manager.getInactiveAvatarEl();
+    inactiveAvatarEl;
+    inactiveAvatarEl.setAttribute( 'camera', 'active', true ); //Should auto-shutoff active camera.
   },
   init: function() { 
     var _self = this;
