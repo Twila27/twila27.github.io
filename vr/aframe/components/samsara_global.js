222e139f80e8feb15d3d52ac073274e15d637574
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-18 05:23:34 -0600

    Update samsara_global.js

--- vr/aframe/components/samsara_global.js
+++ vr/aframe/components/samsara_global.js
@@ -1,21 +1,25 @@
 AFRAME.registerComponent( 'samsara_global', {
   getActiveAvatarEl: function() {
-        var keysCameraEl = document.querySelector('#keysWorldCamera');
+        var sceneEl = this.el.sceneEl;
+    
+        var keysCameraEl = sceneEl.querySelector('#keysWorldCamera');
         if ( keysCameraEl.components.camera.data.active )
           return keysCameraEl;
 
-        var foesCameraEl = document.querySelector('#foesWorldCamera');
+        var foesCameraEl = sceneEl.querySelector('#foesWorldCamera');
         if ( foesCameraEl.components.camera.data.active )
           return foesCameraEl;
 
         return undefined;
   },
   getInactiveAvatarEl: function() {
-        var keysCameraEl = document.querySelector('#keysWorldCamera');
+        var sceneEl = this.el.sceneEl;
+    
+        var keysCameraEl = sceneEl.querySelector('#keysWorldCamera');
         if ( !keysCameraEl.components.camera.data.active )
           return keysCameraEl;
 
-        var foesCameraEl = document.querySelector('#foesWorldCamera');
+        var foesCameraEl = sceneEl.querySelector('#foesWorldCamera');
         if ( !foesCameraEl.components.camera.data.active )
           return foesCameraEl;
 
