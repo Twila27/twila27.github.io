d0e1f68f727367f96af3847634f49ae70664d543
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-21 16:13:17 -0600

    Update foes.js

--- vr/aframe/components/foes.js
+++ vr/aframe/components/foes.js
@@ -1,6 +1,6 @@
 AFRAME.registerComponent( 'foe', {
   schema: {
-    nodePositions: { type : 'vec3' },
+    nodePositions: { type : 'array' },
     numNodes: { default: 3 },
     numLives: { default: -1 },
     dieSFX: { type: 'string' },
