c4d7016c31f17834c616f0036945abbca4c65ddf
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-24 01:11:02 -0600

    Update waypoints.js

--- vr/aframe/components/waypoints.js
+++ vr/aframe/components/waypoints.js
@@ -1,45 +1,30 @@
 AFRAME.registerComponent( 'cursor-listener', {
-  getActiveAvatarEl: function( self ) { 
-    return self.el.sceneEl.components.samsara_global.getActiveAvatarEl();
-  }, 
-  movePlayerToLocation: function( self, worldSpaceLocation ) {
-    var activeAvatarEl = self.getActiveAvatarEl( self );
+  movePlayerToLocation: function( worldSpaceLocation, activeAvatarEl ) {
     var avatarHeight = activeAvatarEl.getAttribute( 'position' ).y; //Preserving it, as worldSpaceLocation is at floor level.
     activeAvatarEl.setAttribute( 'position', { x:worldSpaceLocation.x, y:avatarHeight, z:worldSpaceLocation.z } );
   },
-  createWaypoint: function( self, location ) {
-    var newWaypointElement = self.el.sceneEl.components.pool__waypoints.requestEntity();
-    console.log("Created new waypoint #" + this.numWaypoints + "." );
-    ++this.numWaypoints;
+  createWaypoint: function( location, globalManagerComponent ) {
+    var newWaypointElement = this.el.sceneEl.components.pool__waypoints.requestEntity();
+    console.log("Created new waypoint #" + globalManagerComponent.numWaypoints + "." );
+    ++globalManagerComponent.numWaypoints;
     newWaypointElement.setAttribute( 'position', location );
   },
-  handleClick: function(ev, self) { 
+  handleClick: function( ev, globalManagerComponent ) { 
     var hitObjectLocation = ev.detail.intersection.point;
     var hitObjectClass = ev.detail.intersection.object.el.className;    
 
     if ( hitObjectClass === 'floor' )
     {
-      var activeAvatarEl = self.getActiveAvatarEl( self );
+      var activeAvatarEl = globalManagerComponent.getActiveAvatarEl();
       if ( activeAvatarEl.id === 'keysWorldCamera' )
       {
-        self.createWaypoint( self, hitObjectLocation ); //Else assume it's an existing waypoint.
-        self.movePlayerToLocation( self, hitObjectLocation );
+        this.createWaypoint( hitObjectLocation, globalManagerComponent ); //Else assume it's an existing waypoint.
+        this.movePlayerToLocation( hitObjectLocation, globalManagerComponent, activeAvatarEl );
       }
     }
     else if ( hitObjectClass === 'waypoint' )
     {
-      self.movePlayerToLocation( hitObjectLocation );
-    }
-  },
-  init: function() { 
-    //This if-guard helps prevent addEventListener from running multiple times,
-    //Because there's no way to move the cursor between camera parent elements without reinitializing components.
-    if ( this.numWaypoints === undefined )
-    {
-      this.numWaypoints = 0;
- 
-      var _self = this; //Have to be sure to do this to self-ref the handleClick func below.
-      this.el.addEventListener( 'click', function(event) { _self.handleClick(event, _self); } );
+      this.movePlayerToLocation( hitObjectLocation );
     }
   }
 } );
