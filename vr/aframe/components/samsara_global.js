901a31a90b9b2793fc6334df682761ede19eda38
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-24 01:03:25 -0600

    Update samsara_global.js

--- vr/aframe/components/samsara_global.js
+++ vr/aframe/components/samsara_global.js
@@ -33,5 +33,19 @@ AFRAME.registerComponent( 'samsara_global', {
           return foesCameraEl;
 
         return undefined;
+  },
+  handleClick: function(event, self) {
+    //The reason we add this here, not on cursor-listener anymore:
+    //When an entity element's reparented, its components get remade.
+    //So tracking state there's pointless, and adding listeners seems to weirdly pile up more and more of them.
+    
+    var cursorEl = document.querySelector( '#cursor' ); //Instead, grab the cursor here, wherever it resides.
+    cursorEl.handleClick( event, self ); //Passes self since we potentially ++numWaypoints.
+  },
+  init: function() {
+   this.numWaypoints = 0;
+   
+   var self = this; //Because below listener's scope != this function's.
+   this.el.addEventListener( 'click', function(event) { this.handleClick( event, self ); } );
   }
 } );
