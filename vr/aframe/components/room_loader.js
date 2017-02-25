6fcca47eb04d06e55e9b303c6d9c271d1314aa19
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-25 03:23:55 -0600

    Update room_loader.js

--- vr/aframe/components/room_loader.js
+++ vr/aframe/components/room_loader.js
@@ -2,6 +2,8 @@ AFRAME.registerComponent( 'room_loader', { //If we use hyphens, can't access as 
     schema: {
       roomData : { default : "",
                    parse : function(value) {
+                     if ( value === "" )
+                         return "";
                      var roomDataObj = JSON.parse( value );
                      return roomDataObj;
                    }
