b47b051a71a78661ca4ba35eb354860770d66570
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-25 04:15:56 -0600

    Update room_loader.js

--- vr/aframe/components/room_loader.js
+++ vr/aframe/components/room_loader.js
@@ -1,43 +1,32 @@
 AFRAME.registerComponent( 'room_loader', { //If we use hyphens, can't access as "node.room-loader."
     schema: {
-      roomData : { default : "",
-                   parse : function(jsonPath) {
-                     if ( jsonPath === "" )
-                         return "";
-                     
-                     function loadJSON(path)
-                     {
-                       var success = function(data) { console.log(data); };
-                       var error = function(xhr) { console.error(xhr); };
+      roomDataPath : { default : "" }
+    },
+    loadJSON : function(path)
+    {
+       var success = function(data) { console.log(data); };
+       var error = function(xhr) { console.error(xhr); };
 
-                       var xhr = new XMLHttpRequest();
-                       xhr.onreadystatechange = function()
-                       {
-                         if (xhr.readyState === XMLHttpRequest.DONE) {
-                           if (xhr.status === 200) {
-                             if (success) {
-                               success(JSON.parse(xhr.responseText));
-                             }
-                           } else {
-                             if (error) {
-                               error(xhr);
-                             }
-                           }
-                         }
-                       };
-                       xhr.open("GET", path, true);
-                       xhr.send();
-                     }
-                     
-                     var jsonData = loadJSON( jsonPath );
-                     var roomDataObj = JSON.parse( jsonPath );
-                     return roomDataObj;
-                   },
-                   stringify : JSON.stringify
-                 }
+       var xhr = new XMLHttpRequest();
+       xhr.onreadystatechange = function()
+       {
+         if (xhr.readyState === XMLHttpRequest.DONE) {
+           if (xhr.status === 200) {
+             if (success) {
+               success(JSON.parse(xhr.responseText));
+             }
+           } else {
+             if (error) {
+               error(xhr);
+             }
+           }
+         }
+       };
+       xhr.open("GET", path, true);
+       xhr.send();
     },
     init: function() {
-      console.log(this.data.roomData);
+      this.loadJSON(this.data.roomDataPath);
     }
   }
 );
