646bb507aea9a0370c15f74303e5dc5adbd1b955
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-27 00:25:27 -0600

    Update world_swapper.js

--- vr/aframe/components/world_swapper.js
+++ vr/aframe/components/world_swapper.js
@@ -13,20 +13,44 @@ AFRAME.registerComponent( 'world-swapper', { //Make this the mouseover-slowly-sp
     newActiveCameraEl.appendChild(cursorEl);
   },
   schema: {
-    isKeysWorld : { type : 'boolean' } //Else assume it's the other world.
+    isKeysWorld : { type : 'boolean' }, //Else assume it's the other world.
+    initialDecayBarMax : { default : 100.0 }
   },
   init: function() { 
     var self = this;
     this.el.addEventListener( 'click', function() { self.swapWorlds( self ); } );
+    
+    this.decayBarCurrentMax = this.data.initialDecayBarMax;
+    this.decayBarCurrentValue = this.decayBarCurrentMax;
     
     if ( this.data.isKeysWorld )
       this.followedAvatar = this.el.sceneEl.querySelector('#keysWorldCamera');
     else //Assumption it's the other world's swap button.
       this.followedAvatar = this.el.sceneEl.querySelector('#foesWorldCamera');
   },
+  setDecayMax: function(newVal) {
+    this.decayBarCurrentMax = newVal;
+  },
+  addToDecayMax: function(delta) {
+    this.decayBarCurrentMax += delta;
+  },
+  setDecayValue: function(newVal) {
+    this.decayBarCurrentValue = newVal;
+  },
+  addDecay: function(healDelta) {
+    if ( ( this.decayBarCurrentValue + healDelta ) <= this.decayBarCurrentMax )
+      this.decayBarCurrentValue += healDelta;
+  },
+  removeDecay: function(decayDelta) {
+    if ( ( this.decayBarCurrentValue - decayDelta ) > 0 )
+      this.decayBarCurrentValue -= decayDelta;
+  },
   tick: function() {
     var newPosition = this.followedAvatar.getAttribute( 'position' ); //Can't store reference to array, only object (itself a ref).
     const HEIGHT_OFFSET_FROM_CAMERA = 2;
     this.el.setAttribute( 'position', { x:newPosition.x, y:newPosition.y + HEIGHT_OFFSET_FROM_CAMERA, z:newPosition.z } );
+    
+    var currentTheta = ( this.decayBarCurrentValue / this.decayBarCurrentMax ) * 360.0;
+    this.el.setAttribute( 'geometry', 'thetaLength', currentTheta );
   }
 } );
