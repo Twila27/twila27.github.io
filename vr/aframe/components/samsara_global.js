cd37ec69f9512186afa477f5e6c78419ce7af677
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-27 08:38:58 -0600

    Update samsara_global.js

--- vr/aframe/components/samsara_global.js
+++ vr/aframe/components/samsara_global.js
@@ -171,8 +171,11 @@ AFRAME.registerComponent( 'samsara_global', {
    for ( const soundName in soundArray ) //'this' ref has different scope in here?
    {
      var componentName = self.getSoundAttributeNameForSchemaProperty(soundName);
-     self.speakerEl.setAttribute( componentName );
-     self.speakerEl.setAttribute( componentName, 'src', soundArray[soundName] );
+     self.speakerEl.setAttribute( componentName, {
+       src: soundArray[soundName],
+       poolSize: 1,
+       volume: 1
+     });
      self.speakerEl.components[ componentName ].init();
    }    
   },
