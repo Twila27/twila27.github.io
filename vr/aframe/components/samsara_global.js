91dce27dd99b24d35853322da887187548115e2e
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-27 17:55:42 -0600

    Update samsara_global.js

--- vr/aframe/components/samsara_global.js
+++ vr/aframe/components/samsara_global.js
@@ -175,8 +175,7 @@ AFRAME.registerComponent( 'samsara_global', {
        poolSize: 1,
        volume: 1
      });
-     //self.speakerEl.components[ componentName ].init();
-     //self.speakerEl.components[ componentName ].setupSound();
+     self.speakerEl.components[ 'componentName' ].multiple = false;
    }    
   },
   tick: function( time, timeDeltaMilliseconds )
