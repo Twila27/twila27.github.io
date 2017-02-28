90addde7993a9b3b78f0289384744d9988a37d85
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-27 19:08:24 -0600

    Update room_loader.js

--- vr/aframe/components/room_loader.js
+++ vr/aframe/components/room_loader.js
@@ -69,10 +69,11 @@ AFRAME.registerComponent( 'room_loader', //If we use hyphens, can't access as "n
             doorRoomID: newRoomID,
             showNodeImmediately: !this.hasSpawnedDoor //Ensures we only do this for first room.
           };
-          properties.isKeysWorld = false;
           foesWorldEl.setAttribute( 'door_opener', properties );
-          properties.isKeysWorld = true;
+          foesWorldEl.setAttribute( 'door_opener', 'isKeysWorld', false );
           keysWorldEl.setAttribute( 'door_opener', properties );
+//          properties.isKeysWorld = true;
+          foesWorldEl.setAttribute( 'door_opener', 'isKeysWorld', true );
           break;
         case 'spawner': //A lot of "if JSON has it, defer to that, else use member method's mapped defaults."
           var properties = {
