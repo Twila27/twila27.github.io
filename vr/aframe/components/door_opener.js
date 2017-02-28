df7c9db1f2f85d0551f06d35cb59f5bed4f87684
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-27 18:54:34 -0600

    Update door_opener.js

--- vr/aframe/components/door_opener.js
+++ vr/aframe/components/door_opener.js
@@ -33,7 +33,7 @@ AFRAME.registerComponent( 'door_opener' , {
     if ( this.data.doorRoomID == -1 )
       console.log("No doorRoomId given to schema by room_loader!");
     else
-      this.el.emit( 'door_opened', this.data.doorRoomID, this.data.isKeysWorld );
+      this.el.emit( 'door_opened', { doorRoomID : this.data.doorRoomID, isKeysWorld: this.data.isKeysWorld } );
     
     var currPos = this.el.getAttribute('position');
     this.el.setAttribute( 'animation', {
