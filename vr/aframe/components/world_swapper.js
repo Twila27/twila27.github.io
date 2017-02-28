1dd024304feb2977f2a1f8440ffe380bbde47490
Benjamin D. Gibson <benjamin.gibson@okstate.edu>
2017-02-27 23:34:49 -0600

    Update world_swapper.js

--- vr/aframe/components/world_swapper.js
+++ vr/aframe/components/world_swapper.js
@@ -9,9 +9,7 @@ AFRAME.registerComponent( 'world-swapper', { //Make this the mouseover-slowly-sp
     var manager = self.el.sceneEl.components.samsara_global;
     var oldActiveCameraEl = manager.getActiveAvatarEl();
     var newActiveCameraEl = manager.getInactiveAvatarEl();
-    newActiveCameraEl.setAttribute( 'camera', 'active', true ); //Runs auto-off on old active camera.
     
-    //Now also move the cursor (component, not entity anymore because reattributing > reparenting) over.
     var oldCursorEl = oldActiveCameraEl.querySelector('#cursor');
     var newCursorEl = newActiveCameraEl.querySelector('#cursorNextSwap');
     oldCursorEl.removeAttribute( 'cursor' );
@@ -19,7 +17,9 @@ AFRAME.registerComponent( 'world-swapper', { //Make this the mouseover-slowly-sp
     oldCursorEl.id = 'cursorNextSwap';
     newCursorEl.setAttribute( 'cursor', {} );
     newCursorEl.id = 'cursor';
-    
+
+    newActiveCameraEl.setAttribute( 'camera', 'active', true ); //Runs auto-off on old active camera.
+
     //We want this sound playing on both, so when we warp over we don't lose the sound.
     this.playSound("swapActivating", this.el.object3D.getWorldPosition() );
     if ( this.data.isKeysWorld )
